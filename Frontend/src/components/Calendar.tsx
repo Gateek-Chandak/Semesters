9460c0be0d7448a5e94a835e27f0eb8399d46e6a
Gateek <chandakgateek@gmail.com>
2025-01-05 00:18:25 -0500

    fixed all typesript errors

--- Frontend/src/components/Calendar.tsx
+++ Frontend/src/components/Calendar.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
+import { cn } from '../lib/utils';
 import { VariantProps, cva } from 'class-variance-authority';
 import {
   Locale,
