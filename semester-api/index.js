9d67548288bf7b4256a7dd0f18705c00f93c4790
Gateek <chandakgateek@gmail.com>
2025-01-06 18:32:38 -0500

    new api again...

--- semester-api/index.js
+++ semester-api/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
 
 const app = express()
 const PORT = 8000
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.status(200).send('Hello World')
 })
 
 app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
+  res.status(200).send('About route 🎉 ')
 })
 
 app.listen(PORT, () => {
