0b97f79d72f659f87c7d22166f5d51682639188f
Gateek <chandakgateek@gmail.com>
2025-01-06 18:18:20 -0500

    created new api

--- semester-api/app.js
+++ semester-api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express()
 const PORT = 8000
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -14,3 +18,5 @@ app.get('/about', (req, res) => {
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 })
+
+module.exports = app;
