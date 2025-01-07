a32e3cb5608b4f345c9cc8ef883846b463e6aa71
Gateek <chandakgateek@gmail.com>
2025-01-06 19:36:31 -0500

    copied old api

--- semester-api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('hello'))
-
-app.listen(4000, () => console.log('app running on port 4000'))
-
-module.exports = app;
